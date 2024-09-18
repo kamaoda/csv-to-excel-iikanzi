babee10ab5af613d144a6e46fbe6b7f9534582ee
kamaoda <kamada.1994@gmail.com>
2024-09-18 22:47:12 +0900

    ガワだけ作成した

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 'use client'
 
-import { Button, Flex, Heading, Input, useColorMode, useColorModeValue } from "@chakra-ui/react";
+import { Button, Flex, Heading, Input, useColorModeValue } from "@chakra-ui/react";
 
 export default function Home() {
-  const { colorMode, toggleColorMode } = useColorMode();
   const formBackground = useColorModeValue("gray.100", "gray.700");
   return (
     <Flex 
-      height="100dvh"
+      minHeight="100dvh"
       alignItems="center"
       justifyContent="center"
     > 
@@ -17,11 +16,9 @@ export default function Home() {
         p="12"
         rounded={6}
       >
-        <Heading mb={6}>Hello World</Heading>
-        <Input placeholder="yamada@example.com" variant="filled" mb={2} type="email" />
-        <Input placeholder="******"             variant="filled" mb={4} type="password" />
-        <Button colorScheme="teal">Log in</Button>
-        <Button colorScheme="teal" onClick={toggleColorMode} mt={4}>Toggle Color Mode</Button>
+        <Heading mb={6}>CSVをいい感じの.xlsxに変換する君</Heading>
+        <Input variant="filled" mb={2} type="file" />
+        <Button colorScheme="teal">変換！</Button>
       </Flex>
     </Flex>
   );
